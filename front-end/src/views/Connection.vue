7f62d3c8fabf3a9e98356823d8e254f54c414764
Kyowdem <collomb.mehdi@gmail.com>
2021-11-08 19:14:33 -0500

    add vue-router to project

---
+++ front-end/src/views/Connection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- <img alt="Vue logo" src="./../public/ClassMeetLogo.png" /> -->
+
+    <div class="connection">
+      <h1>TITRE</h1>
+    </div>
+    <div>
+      <form action="login">
+        <label>Adresse courriel ou identifiant du compte</label><br />
+        <input type="text" name="username" /><br />
+        <label>Mot de passe :</label><br />
+        <input type="password" name="password" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Connection",
+};
+</script>
+
+<style scoped>
+</style>
+
+
+
+

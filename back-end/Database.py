723b68cb1ecf16a21c6bee7181d2db68122f3e08
JulesHauchecorne <youle21@yahoo.ca>
2021-11-22 14:27:14 -0500

    Affichage de la DB en print

--- back-end/Database.py
+++ back-end/Database.py
@@ -7,23 +7,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy( app )
 
-class etudiant(db.Model):
+# Etudiant
+class Etudiant(db.Model):
     id = db.Column(db.Integer , primary_key = True)
     username = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(20), unique=True, nullable=False)
 
-
+# Permet de sortir en string des éléments de l'objets passé en parametre
 def __repr__(self):
-    return '<etudiant %r %r>' % self.username % self.email
+    return 'Username  %r' % self.username + '\n Email %r' % self.email
 
 db.create_all()
 
-
-### SQLAlchemy define a constructor already
-#def __init__(self, username, email):
-#    self.username = username
-#    self.email = email
+# Print passe la liste passé en paramêtre dans une boucle qui affiche le contenu avec repr
+def print_etudiant(Etudiants):
+    for Etudiant in Etudiants :
+       print( __repr__(Etudiant))
 
 
 #@app.route('/')
--- back-end/main.py
+++ back-end/main.py
@@ -1,9 +1,18 @@
 import Database 
 
 #create object Etudiant that can go in Db
-jules = Database.etudiant(username='jules',email='youle21' , password='xxx')
-#print( repr(jules) )
+#jules = Database.etudiant(username='jules',email='youle21' , password='xxx')
+#Nami = Database.Etudiant(username='Nami2122',email='emailfoo' , password='xXxx')
+#Mehdi = Database.Etudiant(username='Mehdi22',email='emailfoo2' , password='xXx')
+
 
 # Need to add and commit to save it in Db
-Database.db.session.add(jules)
-Database.db.session.commit()
+#Database.db.session.add(Mehdi)
+#Database.db.session.commit()
+
+
+# Accède à la db pour faire une liste actuelle des étudiants dans la table étudiant
+list_etudiants = Database.Etudiant.query.all()
+
+Database.print_etudiant(Etudiants=list_etudiants)
+

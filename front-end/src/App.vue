22d14a9a6409d69d3cd4ec15e94b9254911e0b04
Kyowdem <collomb.mehdi@gmail.com>
2021-11-22 19:21:59 -0500

    router-view

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,25 +1,13 @@
 <template>
   <div id="app">
-        <router-view/>
-        <Barre_de_navigation/>
-        <Connexion msg="Connectez-vous à "/>
-        <Footer/>
-    </div>
+    <router-view />
+  </div>
 </template>
 
 <script>
-import Connexion from './components/Connexion.vue'
-import Barre_de_navigation from "./components/Barre_de_navigation";
-import Footer from "./components/Footer";
-
 export default {
-  name: 'App',
-  components: {
-    Barre_de_navigation,
-    Connexion,
-    Footer,
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
@@ -30,5 +18,4 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-
 </style>

042d6ac42acadd151a9766f878c9b3673f325318
Kyowdem <collomb.mehdi@gmail.com>
2021-11-22 19:55:03 -0500

    if no login go to login page

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -7,10 +7,27 @@
 <script>
 export default {
   name: "App",
+  beforeCreate() {
+    if (
+      this.$router.currentRoute.fullPath !== "/login" &&
+      !this.$store.state.session_id
+    ) {
+      this.$router.push("/login");
+    } else if (
+      this.$router.currentRoute.fullPath === "/login" &&
+      this.$store.state.session_id
+    ) {
+      this.$router.push("/");
+    }
+  },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Sen, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;

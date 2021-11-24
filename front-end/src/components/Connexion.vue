b92b20f0b28fd94809b0e454ce3b1cc6d9785a9d
Kyowdem <collomb.mehdi@gmail.com>
2021-11-23 22:27:39 -0500

    catch error

--- front-end/src/components/Connexion.vue
+++ front-end/src/components/Connexion.vue
@@ -42,6 +42,8 @@ export default {
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
+        }).catch(err => {
+          console.error(err)
         });
     },
   },

ec57fb2e96c70ba3c644a12ea271c0255af520ad
JulesHauchecorne <youle21@yahoo.ca>
2021-11-23 21:05:03 -0500

    testing mess

--- back-end/Database.py
+++ back-end/Database.py
@@ -5,26 +5,33 @@ from sqlalchemy.ext.declarative import declarative_base
 
 #Base = declarative_base()
 
-# Forum associative table
-inscription = Table('association', db.Model.metadata, 
-    Column('left_id', ForeignKey('left_id')),
-    Column('right_id', ForeignKey('right_id'))
-        )
+
+
+class Inscription(db.Model):
+    __tablename__= 'inscription'
+    etudiant_id = Column(ForeignKey('etudiant_.id'), primary_key=True)
+    sigle_id = Column(ForeignKey('sigle_.id'), primary_key=True)
+
+    postMaybe = Column(db.String(200))
+
+    sigle = relationship('Sigle', backref=('etudiant_inscription'))
+    etudiant = relationship('Etudiant',  backref=('sigle_inscription'))
+
 
 # Sigle cours
 class Sigle(db.Model):
-    __tablename__ = 'right'
+    __tablename__ = 'sigle_'
     id = db.Column(db.Integer , primary_key = True)
     sigle = db.Column(db.String(7))
-    eutdiants_inscrits = relationship("Etudiant", secondary=inscription)
 
 # Etudiant
 class Etudiant(db.Model):
-    __tablename__ = 'left'
+    __tablename__ = 'etudiant_'
     id = db.Column(db.Integer , primary_key = True)
     username = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(20), unique=True, nullable=False)
+#    cours = relationship('Inscription')
 
 
     # Permet de sortir en string des éléments de l'objets passé en parametre
@@ -32,21 +39,14 @@ def __repr__(self):
     return 'Username  %r' % self.username + '\n Email %r' % self.email
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 db.create_all()
+
 
 # Print passe la liste passé en paramêtre dans une boucle qui affiche le contenu avec repr
 def print_etudiant(Etudiants):
     for Etudiant in Etudiants :
        print( __repr__(Etudiant))
+
+def print_inscription(etudiant):
+    for  assoc in etudiant.cours :
+        print( assoc.sigle )
--- back-end/main.py
+++ back-end/main.py
@@ -2,18 +2,36 @@ import Database
 import server
 
 #create object Etudiant that can go in Db
-#jules = Database.etudiant(username='jules',email='youle21' , password='xxx')
+Jules = Database.Etudiant(username='jules',email='youle21' , password='xxx')
 #Nami = Database.Etudiant(username='Nami2122',email='emailfoo' , password='xXxx')
 #Mehdi = Database.Etudiant(username='Mehdi22',email='emailfoo2' , password='xXx')
+#
+#
+#inf5151 = Database.Sigle(sigle='INF5151')
+#inf5171 = Database.Sigle(sigle='INF5171')
+#inf5153 = Database.Sigle(sigle='INF5153')
 
-
-# Need to add and commit to save it in Db
+## Need to add and commit to save it in Db
+#Database.db.session.add(Jules)
+#Database.db.session.add(Nami)
+#Database.db.session.add(inf5151)
+#Database.db.session.add(inf5171)
+#Database.db.session.add(inf5153)
 #Database.db.session.add(Mehdi)
 #Database.db.session.commit()
 
 
 # Accède à la db pour faire une liste actuelle des étudiants dans la table étudiant
 list_etudiants = Database.Etudiant.query.all()
+##list_inscriptions = Database.Inscription.query.all()
+#test = Database.Inscription()
+#test.sigle = Database.Sigle.query.all()
+#Jules.cours.append(test)
+#Database.Inscription.sigle = Database.Sigle
+#Jules.cours.append( Database.Inscription.sigle )
+#Database.print_etudiant(Etudiants=list_etudiants)
 
-Database.print_etudiant(Etudiants=list_etudiants)
 
+#for assoc in Jules.cours:
+#    print(assoc.sigle)
+#Database.print_inscription( Jules )

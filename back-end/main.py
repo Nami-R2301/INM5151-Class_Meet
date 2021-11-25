e943742253f0554c359c8cc065f6aa655cc145be
JulesHauchecorne <youle21@yahoo.ca>
2021-11-25 18:58:35 -0500

    ajout de liste d'objets Publication avec le contenu

--- back-end/main.py
+++ back-end/main.py
@@ -11,7 +11,7 @@ data.add_testing_data()
 # Accède à la db pour faire une liste actuelle des étudiants dans la table étudiant
 list_etudiants = Database.Etudiant.query.all()
 # Méthode qui renvois les éleves inscrits
-i.print_etudiant(Etudiants=list_etudiants)
+#i.print_etudiant(Etudiants=list_etudiants)
 # affiche les publication du la catégorie cours INF5151
 i.print_publications( 'INF5151' )
 

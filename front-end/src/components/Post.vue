0d4fed72950454482918cb3f4b896fc15543d9f5
Kyowdem <collomb.mehdi@gmail.com>
2021-11-25 18:51:26 -0500

    create forum

---
+++ front-end/src/components/Post.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="row justify-content-between">
+      <div class="col-3">
+        <p>{{ auteur }}</p>
+      </div>
+      <div class="col-3">
+        <p>{{ dateTime }}</p>
+      </div>
+    </div>
+    <div class="row justify-content-start">
+      <div class="col-12">
+        <p>{{ contenu }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    auteur: String,
+    contenu: String,
+    dateTime: String,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ front-end/src/views/Forum.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">FORUM</div>
+    </div>
+    <div class="row posts">
+      <Post
+        v-for="post in posts"
+        :key="post.dateTime + '_' + generateHexString()"
+        :auteur="post.auteur"
+        :contenu="post.contenu"
+        :dateTime="post.dateTime"
+      ></Post>
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from "../components/Post.vue";
+
+export default {
+  components: {
+    Post,
+  },
+  data: () => ({
+    posts: [],
+  }),
+  mounted() {
+    this.getPost();
+  },
+  methods: {
+    // Used only to make the post unique.
+    generateHexString() {
+      return [...Array(20)]
+        .map(() => Math.floor(Math.random() * 16).toString(16))
+        .join("");
+    },
+    getPost() {
+      this.posts = [
+        {
+          auteur: "name",
+          contenu: "Lorem ipsium",
+          dateTime: new Date().toString(),
+        },
+        {
+          auteur: "name",
+          contenu: "Lorem ipsium",
+          dateTime: new Date().toString(),
+        },
+        {
+          auteur: "name",
+          contenu: "Lorem ipsium",
+          dateTime: new Date().toString(),
+        },
+      ];
+    },
+  },
+};
+</script>
+
+<style>
+</style>
